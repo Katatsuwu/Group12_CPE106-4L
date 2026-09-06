0ff2e5db87c687ac5f9d424c0a6b7034b14f38fd
Katatsuwu <120778902+Katatsuwu@users.noreply.github.com>
2026-09-06 19:45:18 +0800

    Delete Lab3/main.py

--- Lab3/main.py
+++
@@ -1,8 +0,0 @@
-from customerManagement import CustomerManagement
-from menuManagement import MenuManagement
-from orderProcessing import OrderProcessing
-from paymentDelivery import PaymentAndDelivery
-
-
-class Main:
-    pass
